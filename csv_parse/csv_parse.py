3f30bdb85447af8731b97a66410fd6ae5046cb27
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-24 20:15:47 -0700

    final

--- csv_parse/csv_parse.py
+++ csv_parse/csv_parse.py
@@ -1,17 +1,29 @@
 import csv
 
-# implement most_successful that returns the number of movies published prior to a given year, with associated earnings.
+# returns most successful movie (in earnings) of given year provided
+
+def takeSecond(elem):
+    return int(elem[2])
 
 def most_successful(year):
 
     with open('movies.csv', mode='r') as infile:
         reader = csv.reader(infile)
         mydict = {}
+        filtered_list = []
+
         for rows in reader:
             mydict[rows[0]] = rows[1], rows[2]
         
         for movie in mydict.keys():
-            if mydict[movie][0] == year:
-                print(movie, mydict[movie][0],  mydict[movie][1])
+            if mydict[movie][0] == str(year):
+                filtered_list.append([movie, mydict[movie][0],  mydict[movie][1]])
 
-most_successful("1984")
+        for item in filtered_list:
+            filtered_list.sort(key=takeSecond)
+
+        return filtered_list[-1][0], "Earnings: $" + filtered_list[-1][2]
+
+user_input = input("enter a year: ")
+print("Most successful film of this year: ")
+print(most_successful(user_input))
