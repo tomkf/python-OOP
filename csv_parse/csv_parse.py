e8921f7a321866959f5dd0b3a183834861777c93
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-24 19:55:06 -0700

    update

--- csv_parse/csv_parse.py
+++ csv_parse/csv_parse.py
@@ -1,1 +1,17 @@
+import csv
+
 # implement most_successful that returns the number of movies published prior to a given year, with associated earnings.
+
+def most_successful(year):
+
+    with open('movies.csv', mode='r') as infile:
+        reader = csv.reader(infile)
+        mydict = {}
+        for rows in reader:
+            mydict[rows[0]] = rows[1], rows[2]
+        
+        for movie in mydict.keys():
+            if mydict[movie][0] == year:
+                print(movie, mydict[movie][0],  mydict[movie][1])
+
+most_successful("1984")
