30f718b070d79c46affac286799d7855eca7bb09
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-25 19:05:17 -0700

    Delete user_interface.py

--- user_interface.py
+++
@@ -1,7 +0,0 @@
-from word_locator import request_word, generate_grid
-
-print("********* Welcome to the longest word-game! *********")
-print("Here is your grid:")
-user_grid = generate_grid()
-print("*****************************************************")
-request_word(input("What is your longest word? \n "), user_grid)
