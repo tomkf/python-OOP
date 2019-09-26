3996754bd5eff39f37c57acadbd3d82e5c35c545
Thomas Ferris <thomaskeithferris@gmail.com>
2019-09-25 17:42:30 -0700

    truth statement working

--- user_interface.py
+++ user_interface.py
@@ -1,8 +1,7 @@
 from word_locator import request_word, generate_grid
 
-
 print("********* Welcome to the longest word-game! *********")
 print("Here is your grid:")
-generate_grid()
+user_grid = generate_grid()
 print("*****************************************************")
-request_word(input("What is your longest word? \n "))
+request_word(input("What is your longest word? \n "), user_grid)
--- word_locator.py
+++ word_locator.py
@@ -1,17 +1,29 @@
 import requests 
-
-
+import string
+import random
 
 def generate_grid():
-    print("Q F M R K L I T P")
+    grid = []
+    for i in range(12):
+        grid.append(random.choice(string.ascii_uppercase))    
+    print(*grid)
+    return grid
 
-def request_word(user_word):
+def request_word(user_word, user_gird):
     URL = "https://wagon-dictionary.herokuapp.com/%s" %(user_word)
     # PARAMS = {'address':location} 
     r = requests.get(url = URL) 
     data = r.json() 
-    print(data)
+    compare_grid = list(user_word.upper())
+    final_statement = True
 
+    for item in compare_grid:
+        statement = False
+        if item in user_gird:
+            statement = True
+        if statement == False:
+            final_statement = False
+    
+    print(final_statement)
 
-
-# request_word("dog")
+# def interpret_results(user_dict):
